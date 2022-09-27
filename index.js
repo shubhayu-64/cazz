ce79b31cf1aef50a185b650b8796124f8e558dd5
Shubhayu Majumdar <majumdarshubhayu@gmail.com>
2022-09-28 00:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Nov 13, 2022 15:37:25").getTime();
+var countDownDate = new Date("Oct 16, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
